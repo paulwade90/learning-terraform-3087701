0c4fa9b284ff5a2669c1589deec8db359876b487
paulwade90 <38004455+paulwade90@users.noreply.github.com>
2023-01-07 16:06:12 +0000

    Corrected security group rule

--- main.tf
+++ main.tf
@@ -58,7 +58,7 @@ resource "aws_security_group_rule" "blog_https_in"{
 
 resource "aws_security_group_rule" "blog_everything_out"{
   type        = "egress"
-  from_port   = o
+  from_port   = 0
   to_port     = 0
   protocol    = "-1"
   cidr_blocks =  ["0.0.0.0/0"]
