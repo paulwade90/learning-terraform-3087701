c7546b9913d912f5789e06e40f3061adb94fde8b
Paul Wade <paul@paul-wade.co.uk>
2023-01-08 19:26:12 +0000

    Applied code fix

--- main.tf
+++ main.tf
@@ -34,6 +34,7 @@ module "vpc" {
 resource "aws_instance" "blog" {
   ami           = data.aws_ami.app_ami.id
   instance_type = var.instance_type
+  subnet_id     = module.vpc.public_subnets[0]
 
   vpc_security_group_ids = [module.blog_sg.security_group_id]
 
@@ -47,7 +48,7 @@ module "blog_sg" {
   version = "4.16.2"
   name = "blog"
   
-  vpc_id = module.vpc.public_subnets[0]
+  vpc_id = module.vpc.vpc_id
 
   ingress_rules       = ["http-80-tcp","https-443-tcp"]
   ingress_cidr_blocks = ["0.0.0.0/0"]
